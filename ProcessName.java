afe975ba530f27490d44804f19e2222ac960dbda
Alim Tegar <42492971+alimtegar@users.noreply.github.com>
2020-11-29 21:03:51 +0700

    Update ProcessName.java

--- ProcessName.java
+++ ProcessName.java
@@ -15,13 +15,14 @@ public class ProcessName {
     public static String processName(String name) {
         int iSpace = name.indexOf(" "); // Mengambil posisi indeks dari spasi
         
-        String firstName = name.substring(0, iSpace),               // Mengambil nama depan (karakter dari index awal (0) sampai indeks spasi)
-               lastName = name.substring(iSpace, name.length());    // Mengambil nama belakang ( karakter dari index spasi sampai index terakhir (yg didapat dari name.length())
+        // Mengambil nama depan (karakter dari index awal (0) sampai indeks spasi)
+        String firstName = name.substring(0, iSpace),               
+        
+        // Mengambil nama belakang ( karakter dari index spasi sampai index terakhir (yg didapat dari name.length())
+               lastName = name.substring(iSpace, name.length());    
         name = 
-            lastName +              // Nama belakang
-            ", " + 
-            firstName.charAt(0) +   // Mengambil karakter pertama (karakter dengan indeks 0) dari nama depan
-            ".";
+            lastName + ", " +              // Nama belakang + ,
+            firstName.charAt(0) +  ".";     // Mengambil karakter pertama (karakter dengan indeks 0) dari nama depan + .;
         
         return name;
     }
