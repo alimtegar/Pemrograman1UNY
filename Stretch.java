7e6e40045b6063e69f99bccf38f68a9b818261c2
Alim Tegar <42492971+alimtegar@users.noreply.github.com>
2021-03-26 11:05:36 +0700

    Update Stretch.java

--- Stretch.java
+++ Stretch.java
@@ -1,30 +1,5 @@
-import java.util.ArrayList;
-
-public class Stretch {
+public class Dummy {
     public static void main(String[] args) {
-        ArrayList<Integer> v = new ArrayList<Integer>() {
-            {
-                add(18);
-                add(7);
-                add(4);
-                add(24);
-                add(11);
-            }
-        };
-
-        System.out.println(stretch(v));
-    }
-
-    public static ArrayList<Integer> stretch(ArrayList<Integer> v) {
-        int iCount = v.size() * 2;
-
-        for (int i = 0; i < iCount; i += 2) {
-            int half = v.get(i) / 2;
-
-            v.set(i, v.get(i) - half);
-            v.add(i + 1, half);
-        }
-
-        return v;
+        // ...
     }
 }
